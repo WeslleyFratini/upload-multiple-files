6dd8cb0edbe541d42b352713cdb7a0cc03674cc1
unknown <weeslleey354@gmail.com>
2021-03-03 19:25:52 -0300

    calculo upload

--- upload/frontend/public/app.js
+++ upload/frontend/public/app.js
@@ -1,12 +1,27 @@
+let bytesAmount = 0;
+
+const formatBytes = (bytes, decimals = 2) => {
+  if (bytes === 0) return "0 Bytes";
+};
+
+const updateStatus = (size) => {
+  const text = `Pending Bytes to Upload: <strong>${size}</strong>`;
+  document.getElementById("size").innerHTML = text;
+};
+
 const showSize = () => {
   const { files: fileElements } = document.getElementById("file");
   if (!fileElements.length) return;
 
   const file = Array.from(fileElements);
-  const size = files.recude((prev, next) => ({ size: prev.size + next.size }), {
-    size: 0,
-  });
-  debugger;
+  const { size } = files.reduce(
+    (prev, next) => ({ size: prev.size + next.size }),
+    {
+      size: 0,
+    }
+  );
+  bytesAmount = size;
+  updateStatus(size);
 };
 
 const onload = () => {
@@ -14,3 +29,4 @@ const onload = () => {
 };
 
 window.onload = onload;
+window.showSize = showSize;
