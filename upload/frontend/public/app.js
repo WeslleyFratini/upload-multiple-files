25a568363c35bfdf296150919b30d21892aa73c6
unknown <weeslleey354@gmail.com>
2021-03-03 19:40:02 -0300

    upload arquivo

--- upload/frontend/public/app.js
+++ upload/frontend/public/app.js
@@ -2,10 +2,16 @@ let bytesAmount = 0;
 
 const formatBytes = (bytes, decimals = 2) => {
   if (bytes === 0) return "0 Bytes";
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB"];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + "" + sizes[i];
 };
 
 const updateStatus = (size) => {
-  const text = `Pending Bytes to Upload: <strong>${size}</strong>`;
+  const text = `Pending Bytes to Upload: <strong>${formatBytes(size)}</strong>`;
   document.getElementById("size").innerHTML = text;
 };
 
@@ -22,6 +28,14 @@ const showSize = () => {
   );
   bytesAmount = size;
   updateStatus(size);
+
+  const interval = setInterval(() => {
+    console.count();
+    const result = bytesAmount - 5e6;
+    bytesAmount = result < 0 ? 0 : result;
+    updateStatus(bytesAmount);
+    if (bytesAmount === 0) clearInterval(interval);
+  }, 50);
 };
 
 const onload = () => {
